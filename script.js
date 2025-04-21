042596b3db75174a677b2f82642c2060fbd864a1
jayjuy-tech <jayyyjuyyy@gmail.com>
2025-04-21 07:13:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Script untuk animasi dan interaktivitas tombol
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.gallery-img');
+
+    images.forEach(image => {
+        image.addEventListener('mouseover', () => {
+            image.style.transform = 'scale(1.1)';
+        });
+        image.addEventListener('mouseout', () => {
+            image.style.transform = 'scale(1)';
+        });
+    });
+});
